d3f6a70eea917b4706bac90bc1f7f046c3be3de9
Leoxyzua <apinky549@gmail.com>
2021-08-06 13:30:28 -0500

    Menciones

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import Bot from "./Client";
-new Bot({ intents: 5983 }).start({
+new Bot({
+  intents: 5983,
+  allowedMentions: { parse: ["users"], repliedUser: false },
+}).start({
   options: { error: "log", clear: true },
 });
